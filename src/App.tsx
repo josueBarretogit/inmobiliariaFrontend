a51e2da6df1aba83aa3b3109f16df4f7ffccd3f2
josueBarretogit <josuebarretogit@gmail.com>
2023-10-04 10:51:40 -0500

    implementacion mainAppBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MainAppBar from './components/appBar';
 import { Typography, Rating, Badge } from '@mui/material';
 import { MailSharp } from '@mui/icons-material';
 
@@ -10,38 +11,10 @@ function App() {
 
   return (
 
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Badge badgeContent={4} color="secondary">
-          <MailSharp />
-        </Badge>
-        <Typography component="legend">Controlled</Typography>
-        <Rating
-          name="simple-controlled"
-          value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }}
-        />
-        <Typography component="legend">Read only</Typography>
-        <Rating name="read-only" value={value} readOnly />
-        <Typography component="legend">Disabled</Typography>
-        <Rating name="disabled" value={value} disabled />
-        <Typography component="legend">No rating given</Typography>
-        <Rating name="no-value" value={null} />
-      </header>
+    <div >
+
+      <MainAppBar />
+
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider } from '@mui/system';
+import { grey, orange } from '@mui/material/colors';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const customTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    grey: grey[900],
+
+    primary: {
+      main: grey[900]
+    }
+  }
+})
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+  <ThemeProvider theme={customTheme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
