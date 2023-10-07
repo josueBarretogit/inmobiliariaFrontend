4937bcb4acccbddee80c02fc3644986eac9760d7
josueBarretogit <josuebarretogit@gmail.com>
2023-10-06 22:24:57 -0500

    cambios color tema e implementacion logo

--- src/components/MainAppBar.tsx
+++ src/components/MainAppBar.tsx
@@ -1,61 +1,78 @@
-import { Container, Toolbar, Typography, Box, IconButton, Menu, MenuItem, Button, Tooltip, Avatar, AppBar, styled, alpha, createTheme } from "@mui/material";
-import * as React from 'react';
-import AdbIcon from '@mui/icons-material/Adb';
-import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
-import { blue, grey, lime, purple } from "@mui/material/colors";
-import { ThemeProvider } from "@mui/system";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import {
+  alpha,
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  createTheme,
+  IconButton,
+  Menu,
+  MenuItem,
+  styled,
+  ThemeProvider,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import { grey, purple } from "@mui/material/colors";
+import InputBase from "@mui/material/InputBase";
+import * as React from "react";
+import logoInmobiliaria from "./../img/inmobiliariaLogo.jpg";
 
-const pages = ['Inicio', 'Nosotros', 'Arrendatario'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const pages = ["Inicio", "Nosotros", "Arrendatario", "Pagar arriendo"];
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
+  "&:hover": {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(1),
-    width: 'auto',
+    width: "auto",
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
       },
     },
   },
 }));
 
 function MainAppBar() {
-
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null,
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null,
+  );
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
@@ -75,21 +92,30 @@ function MainAppBar() {
 
   const customTheme = createTheme({
     palette: {
+      mode: "dark",
       primary: {
-        main: grey[900]
+        main: "#030014",
       },
       secondary: purple,
     },
-  })
-
+  });
 
   return (
     <>
       <ThemeProvider theme={customTheme}>
-        <AppBar position="static" >
+        <AppBar position="static" enableColorOnDark>
           <Container maxWidth="xl">
-
-            <Toolbar >
+            <Toolbar>
+              <Avatar
+                alt="Logo inmobiliaria"
+                src={logoInmobiliaria}
+                variant="rounded"
+                sx={{
+                  mr: 2,
+                  width: 1 / 7,
+                  height: 100,
+                }}
+              />
               <Typography
                 variant="h6"
                 noWrap
@@ -97,21 +123,20 @@ function MainAppBar() {
                 href="#app-bar-with-responsive-menu"
                 sx={{
                   mr: 2,
-                  display: { xs: 'none', md: 'flex' },
-                  fontFamily: 'monospace',
+                  display: { xs: "none", md: "flex" },
+                  fontFamily: "monospace",
                   fontWeight: 700,
-                  letterSpacing: '.3rem',
-                  color: 'inherit',
-                  textDecoration: 'none',
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
                 }}
               >
                 Inmobiliaria
               </Typography>
 
-              <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-
+              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
                 <IconButton
-                  size="large"
+                  size="medium"
                   aria-label="account of current user"
                   aria-controls="menu-appbar"
                   aria-haspopup="true"
@@ -119,34 +144,31 @@ function MainAppBar() {
                   color="inherit"
                 >
                   <MenuIcon />
-
                 </IconButton>
 
                 <Menu
                   id="menu-appbar"
                   anchorEl={anchorElNav}
                   anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
+                    vertical: "bottom",
+                    horizontal: "left",
                   }}
                   keepMounted
                   transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
+                    vertical: "top",
+                    horizontal: "left",
                   }}
                   open={Boolean(anchorElNav)}
                   onClose={handleCloseNavMenu}
                   sx={{
-                    display: { xs: 'block', md: 'none' },
+                    display: { xs: "block", md: "none" },
                   }}
                 >
-
                   {pages.map((page) => (
                     <MenuItem key={page} onClick={handleCloseNavMenu}>
                       <Typography textAlign="center">{page}</Typography>
                     </MenuItem>
                   ))}
-
                 </Menu>
               </Box>
 
@@ -155,24 +177,24 @@ function MainAppBar() {
                 noWrap
                 sx={{
                   mr: 2,
-                  display: { xs: 'flex', md: 'none' },
+                  display: { xs: "flex", md: "none" },
                   flexGrow: 1,
-                  fontFamily: 'monospace',
+                  fontFamily: "monospace",
                   fontWeight: 700,
-                  letterSpacing: '.3rem',
-                  color: 'inherit',
-                  textDecoration: 'none',
+                  letterSpacing: ".3rem",
+                  color: "inherit",
+                  textDecoration: "none",
                 }}
               >
                 Inmobiliaria
               </Typography>
 
-              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
                 {pages.map((page) => (
                   <Button
                     key={page}
                     onClick={handleCloseNavMenu}
-                    sx={{ my: 2, color: 'white', display: 'block' }}
+                    sx={{ my: 2, color: "white", display: "block" }}
                   >
                     {page}
                   </Button>
@@ -180,39 +202,13 @@ function MainAppBar() {
               </Box>
 
               <Box sx={{ flexGrow: 0 }}>
-                <Tooltip title="Open settings">
-                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  </IconButton>
-                </Tooltip>
-                <Menu
-                  sx={{ mt: '45px' }}
-                  id="menu-appbar"
-                  anchorEl={anchorElUser}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                  }}
-                  open={Boolean(anchorElUser)}
-                  onClose={handleCloseUserMenu}
-                >
-                  {settings.map((setting) => (
-                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                      <Typography textAlign="center">{setting}</Typography>
-                    </MenuItem>
-                  ))}
-                </Menu>
                 <Search>
                   <SearchIconWrapper>
                     <SearchIcon />
                   </SearchIconWrapper>
                   <StyledInputBase
-                    placeholder="Search…"
-                    inputProps={{ 'aria-label': 'search' }}
+                    placeholder="Buscar…"
+                    inputProps={{ "aria-label": "search" }}
                   />
                 </Search>
               </Box>
@@ -221,7 +217,7 @@ function MainAppBar() {
         </AppBar>
       </ThemeProvider>
     </>
-  )
+  );
 }
 
-export default MainAppBar
+export default MainAppBar;
